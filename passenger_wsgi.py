cd3fb76f6e607aee02a15e17907cb60cbd499af5
Martin del Pino <dellpinos7@gmail.com>
2025-09-28 21:48:56 -0300

    adding passenger 2

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,18 @@
-import sys, os
+import sys
+import os
 
-# Agrega la ruta a tu app Django
-sys.path.insert(0, os.path.dirname(__file__) + "/hermes")
+# Agregar la carpeta de tu proyecto Django al path
+PROJECT_ROOT = os.path.join(os.path.dirname(__file__), "hermes")
+sys.path.insert(0, PROJECT_ROOT)
 
-from passenger_wsgi import application
+# Establecer settings de Django
+os.environ['DJANGO_SETTINGS_MODULE'] = 'hermes.settings'
+
+# Activar virtualenv si lo tenés en el host
+activate_this = os.path.join(os.path.dirname(__file__), 'venv/bin/activate_this.py')
+if os.path.exists(activate_this):
+    with open(activate_this) as f:
+        exec(f.read(), {'__file__': activate_this})
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
